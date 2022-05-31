458e08707631c238f76ccb6bd6c8efb62ea165b2
Daniel Micay <danielmicay@gmail.com>
2022-05-30 22:28:14 -0400

    update base code to 12.2 + update dependencies

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE := talkback
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
 LOCAL_SRC_FILES := prebuilt/talkback-release-unsigned.apk
-LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+LOCAL_OPTIONAL_USES_LIBRARIES := com.google.android.wearable androidx.window.extensions androidx.window.sidecar
 
 include $(BUILD_PREBUILT)
