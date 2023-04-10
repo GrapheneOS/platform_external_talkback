5735742c3a353c448d47ef875d1fe23388303ea6
Daniel Micay <danielmicay@gmail.com>
2023-04-10 02:09:05 -0400

    update base code to 13.0 and overhaul changes

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := talkback
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
-LOCAL_SRC_FILES := prebuilt/talkback-release-unsigned.apk
+LOCAL_SRC_FILES := prebuilt/TalkBack.apk
 LOCAL_OPTIONAL_USES_LIBRARIES := com.google.android.wearable androidx.window.extensions androidx.window.sidecar
 
 include $(BUILD_PREBUILT)
